65fafbc4895bacbb2757cdf3222af07e7e3a5421
Nadia-Ghannem <114573371+Nadia-Ghannem@users.noreply.github.com>
2025-03-08 09:10:13 +0100

    Add files via upload

---
+++ voice.py
@@ -0,0 +1,34 @@
+#rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials.yml
+import requests
+import speech_recognition as sr
+import pyttsx3 
+
+converter = pyttsx3.init() 
+converter.setProperty('rate', 150) 
+converter.setProperty('volume', 0.7) 
+voices = converter.getProperty('voices')
+converter.setProperty('voice', voices[1].id)
+  
+rec = sr.Recognizer()
+
+bot_message = ""
+
+while(True):
+    with sr.Microphone(device_index=0) as source:
+        print("bot is Listening...")
+        message = rec.listen(source)
+
+    try:
+        query = rec.recognize_google(message, language="en-in")
+        print("You Said : {}".format(query))
+        r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message":query})
+
+        for i in r.json():
+            print("bot Said : {}".format(i['text']))
+            bot_message = i['text']
+            converter.say(bot_message)
+            converter.runAndWait() 
+
+    except Exception as e:
+        converter.say(e)
+        converter.runAndWait() 
